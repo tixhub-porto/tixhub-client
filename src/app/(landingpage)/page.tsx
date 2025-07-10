79c9633c6c7c2a1ba10540b0c2cabb143a9c0a25
fathan <fathanfadillah74@gmail.com>
2025-07-10 17:13:56 +0700

    update landing & navbar

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -5,10 +5,51 @@ export default function Home() {
   return (
     <section className={styles.landingPageSection}>
       <Navbar />
-      <div className={styles.mainEvents}>
-        <span className={styles.eventTitle}>New Year Music Event</span>
-        <span>Come Join The Event Now</span>
-        <button className={styles.ticketButton}>Get Ticket Now</button>
+      <div className={styles.mainEventsContainer}>
+        <div className={styles.mainEvents}>
+          <span className={styles.eventTitle}>New Year Music Event</span>
+          <span>Come Join The Event Now</span>
+          <button className="ticketButton">Get Ticket Now</button>
+        </div>
+      </div>
+      <div className={styles.listTicketContainer}>
+        <div className={styles.listTicket}>
+          <h1>Ready to Rock?</h1>
+          <div className={styles.cardContainer}>
+            <div className={styles.card}>
+              <div className={styles.cardContent}>
+                <p className={styles.cardDescription}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <button className={`ticketButton ${styles.ticketButtonCard}`}>Buy Ticket</button>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardContent}>
+                <p className={styles.cardDescription}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <button className={`ticketButton ${styles.ticketButtonCard}`}>Buy Ticket</button>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardContent}>
+                <p className={styles.cardDescription}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <button className={`ticketButton ${styles.ticketButtonCard}`}>Buy Ticket</button>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardContent}>
+                <p className={styles.cardDescription}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <button className={`ticketButton ${styles.ticketButtonCard}`}>Buy Ticket</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,68 @@
+'use client';
 import styles from "@css/navbar/navbar.module.css";
+import { useState, useEffect } from "react";
 export default function Navbar() {
-    return (
+    const [isMobile, setIsMobile] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth < 768);
+    };
+
+    useEffect(() => {
+        menuOpen ? document.body.classList.add("unscroll") : document.body.classList.remove("unscroll");
+        return () => {
+            document.body.classList.remove("unscroll");
+        };
+    }, [menuOpen]);
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    if (isMobile === null) {
+        return null;
+    }
+
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+    return (<>
         <nav className={styles.nav}>
             <div className={styles.logo}>
                 <img src="images/logo.png" alt="Logo" />
             </div>
-            <div className={styles.navLinks}>
-                <a href="#" className="is-active">Home</a>
-                <a href="#">Ticket</a>
-                <a href="#">About Us</a>
+            {isMobile && (
+                <div className={styles.burgerIcon} onClick={toggleMenu}>
+                    <span className={menuOpen ? styles.show : ""}>☰</span>
+                </div>
+            )}
+            <div className={styles.navLinks + " " + (menuOpen ? styles.show : "")}>
+                <div className={styles.links}>
+                    <a href="#" className="is-active">Home</a>
+                    <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/more-than.png" alt="more-than" />
+                </div>
+                <div className={styles.links}>
+                    <a href="#">Ticket</a>
+                    <img width="15" height="15" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/more-than.png" alt="more-than" />
+                </div>
+                <div className={styles.links}>
+                    <a href="#">About Us</a>
+                    <img width="15" height="15" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/more-than.png" alt="more-than" />
+                </div>
+                {isMobile && (
+                    <a className={`${styles.loginButton} ticketButton`}>Login</a>
+                )}
             </div>
-            <div className={styles.username}>
-                <a href="/login">Login</a>
-            </div>
+            {!isMobile && (
+                <div className={styles.username}>
+                    <a href="/login">Login</a>
+                </div>
+            )}
         </nav>
+
+        {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
+    </>
     );
 }
