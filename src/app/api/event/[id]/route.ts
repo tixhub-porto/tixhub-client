8d1c766a8cce626f941c60092a130222b01aadd4
fathan <fathanfadillah74@gmail.com>
2026-01-29 23:33:09 +0700

    update

--- src/app/api/event/[id]/route.ts
+++ src/app/api/event/[id]/route.ts
@@ -3,7 +3,7 @@ import { fetchGetBE } from "@/helper/helper";
 
 export async function GET(
     request: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) {
     const { id } = (await params);
     return fetchGetBE("/getEvent", { id });
