ca192f9ed4650659361c3f94304a086f4227dad9
fathan <fathanfadillah74@gmail.com>
2026-01-31 21:40:24 +0700

    update

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -2,9 +2,17 @@
 
 import Categories from "@/components/Categories";
 import { useParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function CategoriesPage() {
     const params = useParams();
     const selectedCategory: any = params.category;
-    return <Categories selectedCategory={selectedCategory} />;
+    return (
+        <>
+            <Suspense>
+                <Categories selectedCategory={selectedCategory} />;
+            </Suspense>
+        </>
+
+    )
 }
